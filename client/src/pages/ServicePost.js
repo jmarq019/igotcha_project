8a99341be8da8d74855d4865bc84bb2d3de3097c
Jorge Barragan <jmarq19@outlook.com>
2021-12-07 00:17:54 -0800

    committing change

--- client/src/pages/ServicePost.js
+++ client/src/pages/ServicePost.js
@@ -56,19 +56,6 @@ const ServicePost = ({socket}) =>{
 
     return(
         <main className="base-grid home-columns">
-            <nav className="full-width nav-columns distribute-even fit">
-                <Link to="/profile">
-                <button className="btn">Profile</button>
-                </Link>
-                <Link to="/find-service">
-                <button className="btn">Find Service</button>
-                </Link>
-                <Link to="/offer-service">
-                <button className="btn">Offer Service</button>
-                </Link>
-                <button className="btn">Language</button>
-                <button onClick={Auth.logout}className="btn">Logout</button>
-            </nav>
             <section className="edit full-width">
                 <form className="editprof fit stack" style={{margin:"auto", maxWidth:"70%"}}>
                     <h4 className="ed">Service Name: {service.type} </h4>
